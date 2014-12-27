92b2d0d3a3566e970099b4b92f13f5b9b82d7e70
lw4992 <lw4992@gmail.com>
2014-12-27 23:15:54 +0800

    初始化仓库

---
+++ include/base/RoshanApp.h
@@ -0,0 +1,22 @@
+#ifndef ROSHANAPP_H
+#define ROSHANAPP_H
+
+#include "MooseApp.h"
+
+class RoshanApp;
+
+template<>
+InputParameters validParams<RoshanApp>();
+
+class RoshanApp : public MooseApp
+{
+public:
+  RoshanApp(const std::string & name, InputParameters parameters);
+  virtual ~RoshanApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* ROSHANAPP_H */
